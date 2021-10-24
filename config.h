352c80c8feb9b8ce5ce33efee85d10e4572fc093
Yobel Tayson <rotkyobel@gmail.com>
2021-10-24 15:51:36 -0500

    🍱 font size changed

--- config.h
+++ config.h
@@ -7,13 +7,13 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const unsigned int systrayspacing = 3;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Sarasa Mono K:size=11", "JetBrainsMono Nerd Font:size=11", "Blobmoji:pixelsize=11:antialias=true:autohint=true"};
-static const char dmenufont[]       = "Sarasa Mono K:size=11";
+static const char *fonts[]          = { "Sarasa Mono K:pixelsize=13:style=medium", "JetBrainsMono Nerd Font:pixelsize=13:style=medium" };
+static const char dmenufont[]       = "Sarasa Mono K:pixelsize=13:style=medium";
 
 #include "/home/hw/.cache/wal/colors-wal-dwm.h"
 
